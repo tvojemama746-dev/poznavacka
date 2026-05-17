f71f5b35af49a44bd8a704a6b55638eef01d7009
Jiří Bubník <bubnik@datalite.cz>
2026-05-17 18:35:46 +0200

    Refine answer layout

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = "poznavacka-v4";
+const CACHE_VERSION = "poznavacka-v5";
 const APP_SHELL_CACHE = `${CACHE_VERSION}-shell`;
 const RUNTIME_CACHE = `${CACHE_VERSION}-runtime`;
 
